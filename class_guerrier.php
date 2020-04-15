85b83a503903c534d94c729bf55b8d5dc3a72d81
Steven-Blombou <blombouteven@gmail.com>
2020-04-15 12:31:56 +0200

    commit fct lecture seul

--- class_guerrier.php
+++ class_guerrier.php
@@ -11,16 +11,18 @@ class Guerrier {
     private $_degat;
 
 
+
+
 //  Init de la class
 
 public function __construct($nom) {
   echo 'Le constructeur a bien été crée ! <br>';
   $this->_nom = $nom;
-  $this->_life = 150;
-  $this->_force = 200;
+  $this->_life = 2000;
+  $this->_force = 0;
   $this->_magie = 0;
   $this->_experience = 0;
-  $this->_degat = 0;
+  $this->_degat = rand(150,225);
 }
 
 public function getNom() {
@@ -47,20 +49,52 @@ public function getDegat() {
   return $this->_degat;
 }
 
+public function setLife($x) {
+ $this->_life = $x;
+
+}
+
 public function parler() {
   echo 'Je suis un guerrier ! <br>';
+  echo 'je me nomme ';
+  echo $this->_nom;
+  echo '<br>';
+}
+
+public function afficherExperience() {
+  echo 'Le guerrier possede actuellement : ';
+  echo $this->_experience;
+  echo ' xp';
+  echo '<br>';
 }
 
 public function gagnerExperience() {
   $this->_experience =   $this->_experience +10;
-  echo 'Le guerrier viens de gagner de l experience ! ';
+  echo 'Le guerrier viens de gagné de l experience ! ';
   echo  $this->_experience;
+  echo ' xp';
   echo '<br>';
 }
 
+public function afficherDegat() {
+  echo 'Le guerrier a encaisser actuellement : ';
+  echo $this->_degat;
+  echo ' points de degat';
+  echo '<br>';
+}
 
+public function frapperAdversaire($adversaire) {
+  $lifeEnnemis = $adversaire->getLife();
+  $lifeEnnemis = $lifeEnnemis - $this->_degat;
+  $adversaire->setLife($lifeEnnemis);
+  echo $this->_nom." a attaqué ".$adversaire->getNom();
+  echo '<br>';
+}
 
+public function afficher() {
+  echo $this->_nom." a encore ".$this->_life." points de vie <br>";
 
+}
 
 }
 
--- index.php
+++ index.php
@@ -4,6 +4,30 @@ require_once('class_guerrier.php');
 $guerrier = new Guerrier('Kratos');
 $guerrier-> parler();
 $guerrier-> gagnerExperience();
+$guerrier-> afficherExperience();
+$guerrier-> afficherDegat();
 
+$guerrier2 = new Guerrier('King Lothric');
+$guerrier2-> parler();
+
+$i=0;
+while(($guerrier->getLife()>0) && ($guerrier2->getLife()>0)) {
+  $i=$i+1;
+  echo 'tour'.$i;
+  echo '<br>';
+
+  $guerrier-> frapperAdversaire($guerrier2);
+  $guerrier2-> frapperAdversaire($guerrier);
+  $guerrier-> afficher();
+  $guerrier2-> afficher();
+
+}
+
+if($guerrier->getLife()<=0) {
+  echo $guerrier2->getNom(). ' a battu '.$guerrier->getNom();
+
+}else {
+  echo $guerrier2->getNom(). ' a été battu par '.$guerrier->getNom();
+}
 
  ?>
